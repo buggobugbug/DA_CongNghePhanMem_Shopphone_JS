d6bf37d86dd14acfe7ec3328ed860948c977cb6b
unknown <nhnhan2102@gmail.com>
2024-05-10 21:53:11 +0700

    update

--- Backend/src/controllers/apiController.js
+++ Backend/src/controllers/apiController.js
@@ -6,6 +6,101 @@ const {
 } = require('../controllers/homeController');
 //hien thi data thong qua api
 
+
+const getAllProduct = async (req, res) => {
+    try {
+        const [results, fields] = await connection.execute(
+            "SELECT * FROM SANPHAM"
+        );
+
+        //Thêm đường dẫn đầy đủ cho mỗi sản phẩm
+        const productsWithImageUrls = results.map((product) => {
+            return {
+                ...product,
+                imageUrl: `http://localhost:8080/api/v1/img/${product.mota}`,
+            };
+        });
+
+        return res.status(200).json({
+            //message: "ok",
+            data: productsWithImageUrls,
+        });
+    } catch (error) {
+        console.error(error.message);
+        return res.status(500).json({
+            message: "Internal server error!!!",
+            error: error.message,
+        });
+    }
+};
+
+const createProduct = async (req, res) => {
+    let id = req.body.id;
+    let tenSP = req.body.tenSP;
+    let tenloaiSP = req.body.loaisp;
+    let soluong = req.body.soluong;
+    let dungluong = req.body.dungluong;
+    let ram = req.body.ram;
+    let manhinh = req.body.manhinh;
+    let pin = req.body.pin;
+    let giatien = req.body.giatien;
+    let ghichu = req.body.ghichu;
+    let tenNSX = req.body.tenNSX;
+    if (!id || !tenSP || !tenloaiSP || !soluong || !dungluong || !ram || !giatien || !manhinh || !pin || !tenNSX) {
+        return res.status(200).json({
+            message: "missing ",
+        });
+    }
+    await connection.execute(
+        "insert into SANPHAM(id, tenSP, tenloaiSP, tenNSX, soluong, dungluong, ram, giatien, ghichu) values (?,?,?,?,?,?,?,?,?)",
+        [id, tenSP, tenloaiSP, tenNSX, soluong, giatien, dungluong, ram, ghichu]
+    );
+    return res.status(200).json({
+        message: "ok",
+    });
+};
+
+//Xóa dữ liệu
+const deleteProduct = async (req, res) => {
+    const productId = req.params.id;
+    if (!productId) {
+        return res.status(200).json({
+            message: "missing ",
+        });
+    }
+    await connection.execute("delete from SANPHAM where id = ?", [productId]);
+    return res.status(200).json({
+        message: "ok",
+    });
+};
+
+const updateProduct = async (req, res) => {
+    let id = req.body.id;
+    let tenSP = req.body.tenSP;
+    let tenloaiSP = req.body.tenloaiSP;
+    let dungluong = req.body.dungluong;
+    let ram = req.body.ram;
+    let soluong = req.body.soluong;
+    let giatien = req.body.giatien;
+    let manhinh = req.body.manhinh;
+    let pin = req.body.pin;
+    let ghichu = req.body.ghichu;
+    let tenNSX = req.body.tenNSX;
+    if (!id || !tenSP || !tenloaiSP || !soluong || !dungluong || !ram || !giatien || !manhinh || !pin || !tenNSX) {
+        return res.status(200).json({
+            message: "missing ",
+        });
+    }
+    await connection.execute(
+        "update SANPHAM set tenSP = ?, tenloaiSP = ?, tenNSX = ?, dungluong = ?, soluong = ?, giatien = ?, ghichu = ? where id = ?",
+        [tenSP, tenloaiSP, tenNSX, dungluong, soluong, giatien, ghichu, id]
+    );
+    return res.status(200).json({
+        message: "ok",
+    });
+};
+
+
 const getAllUser = async (req, res) => {
     const results = await getUser();
     return res.status(200).json({
@@ -17,4 +112,9 @@ const getAllUser = async (req, res) => {
 
 module.exports = {
     getAllUser,
+    getAllProduct,
+    updateProduct,
+    createProduct,
+    deleteProduct,
+
 };
--- Backend/src/routes/api.js
+++ Backend/src/routes/api.js
@@ -2,9 +2,21 @@ const express = require("express");
 const router = express.Router();
 
 const {
+    //product
+    getAllProduct,
+    createProduct,
+    updateProduct,
+    deleteProduct,
+    //user
     getAllUser,
 } = require("../controllers/apiController");
 
 router.get("/user", getAllUser); // get list of users
+
+//Product
+router.get("/product", getAllProduct);
+router.post("/create-product", createProduct);
+router.put("/update-product", updateProduct);
+router.delete("/delete-product/:id", deleteProduct);
 
 module.exports = router;
